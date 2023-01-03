34ff60716ff86ed6ba295bfe30af7e07e51ffdab
David Rittscher <David.Rittscher@gmail.com>
2023-01-03 14:27:14 -0500

    Changed package and seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,18 +1,23 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 const Campground = require('../models/campground')
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers')
 const campground = require('../models/campground');
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Mongo Connection Open")
-    })
-    .catch(error => {
-        console.log('Oh no an Error NO MONGO');
-        console.log(error);
-    })
-const db = mongoose.connection
+const dbUrl = process.env.DB_URL 
+
+
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Mongo connection error:"));
+db.once("open", () => {
+    console.log("Database connected lets save random data!");
+});
+
 const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
@@ -20,7 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 39)
         const camp = new Campground({
-            author: '63ab240eb3c04e13dc8067d9',
+            author: '63b47afa638df534dfb74736',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ratione amet hic. Dolor quasi doloribus, optio, quam blanditiis incidunt nostrum, delectus accusamus aliquid cumque earum magnam. Illo animi consectetur amet.',
